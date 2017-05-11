480d7c33b4daf338c8afd83b999b5800f35d1cdf
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-05-11 19:40:03 -0300

    Setup WebAPI to OMDB Api Url and develop method to search by name and advanced search

--- src/app/index.api.js
+++ src/app/index.api.js
@@ -7,7 +7,37 @@
   /** @ngInject */
   function WebAPI ($log, API_URL, $http) {
 
-    return {};
+    (function construct() {
+      $log.info('WebAPI running on date: %s', new Date().toISOString())
+    })();
+
+    function requestAPI (params) {
+      params.plot = params.plot || 'full';
+      return $http({
+        method: 'GET',
+        url: API_URL,
+        params: params
+      });
+    }
+
+    function searchByName (name) {
+      var query = { s: name };
+      return requestAPI(query);
+    }
+
+    function advancedFilter (params) {
+      var mapTo = { name: 's', year: 'y', type: 'type' };
+      var query = {};
+      for (var key in params) {
+        if (mapTo[key]) query[mapTo[key]] = params[key]
+      }
+      return requestAPI(query);
+    }
+
+    return {
+      searchByName: searchByName,
+      advancedFilter: advancedFilter
+    };
   }
 
 })();
