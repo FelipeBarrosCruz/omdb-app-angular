fc0e179aa96ed0a6406a9a38626527c2f22e1a46
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-05-12 22:13:12 -0300

    Improve URL pattern to SEO filter and pattern

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -6,18 +6,25 @@
 
 
   /** @ngInject */
-  function HomeController ($log, HomeService) {
+  function HomeController ($log, HomeService, $location) {
     $log.info('HomeController initialized on date: %s', new Date().toISOString());
     var vm = this;
     vm.searchList = [];
     vm.searchData = {
-      name: null,
-      year: null
+      name: $location.search().name || null,
+      year: $location.search().year || null
     };
     vm.doSearch = doSearch;
 
 
+    (function construct() {
+      if (vm.searchData.name) {
+        doSearch();
+      }
+    })();
+
     function doSearch () {
+      $location.search(vm.searchData);
       HomeService.doSearch(vm.searchData, function(err, list) {
         if (err) return;
         $log.info('List', list)
