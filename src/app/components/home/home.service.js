988969ace48e7c99f0d10e5aa6461da50f77ab6f
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-05-19 23:52:04 -0300

    Setup basic offline data pattern

--- src/app/components/home/home.service.js
+++ src/app/components/home/home.service.js
@@ -6,8 +6,36 @@
 
 
   /** @ngInject */
-  function HomeService ($log, WebAPI, async, FALLBACK_POSTER_IMG) {
+  function HomeService ($log, WebAPI, async, FALLBACK_POSTER_IMG, ngDexie) {
     $log.info('HomeService initialized on date: %s', new Date().toISOString());
+    var TABLE_NAME = 'movies';
+
+    function createOfflineData (data, done) {
+      return ngDexie.add(TABLE_NAME, data)
+        .then(function onSave (result) {
+          $log.info('Offline data saved with success', result);
+          if (angular.isFunction(done)) return done(null, result);
+        }).catch(function onError (err) {
+          $log.error('Offline data error: ', err);
+          if (angular.isFunction(done)) return done(err || new Error('Register cannot be created'), null);
+        })
+    }
+
+    function selectOfflineData (imdbID, done) {
+      return ngDexie.getByIndex(TABLE_NAME, 'imdbID', imdbID)
+        .then(function onSelect (result) {
+          $log.info('Offline data select with success', result);
+          return done(null, result);
+        }).catch(function onError (err) {
+          $log.error('Offline data error: ', err);
+          return done(err || new Error('Register doest exists'), null);
+        })
+    }
+
+    function parseResultPoster (result) {
+      if (result.Poster === 'N/A') result.Poster = FALLBACK_POSTER_IMG;
+      return result;
+    }
 
     function parseListResult (data, done) {
       if (!data.Response || !angular.isArray(data.Search)) return done(null, []);
@@ -16,12 +44,22 @@
 
       data.Search.map(function (value) {
         return Tasks.push(function (next) {
-          return searchByImdbId(value.imdbID, function onSearchByIdmbId (err, response) {
-            if (err) return next(err);
-            if (response.Poster === 'N/A') response.Poster = FALLBACK_POSTER_IMG;
-            if (response) result.push(response);
-            return next(null);
-          });
+          return selectOfflineData(value.imdbID, function onResponse (err, response) {
+            if (!err && result) {
+              console.log("\n\n\n")
+              console.log('CUCUCUCU', err, response)
+              console.log("\n\n\n")
+              result.push(parseResultPoster(response))
+              return next(null)
+            }
+            return searchByImdbId(value.imdbID, function onSearchByIdmbId (err, response) {
+              if (err) return next(err);
+              if (response.Poster === 'N/A') response.Poster = FALLBACK_POSTER_IMG;
+              if (response) result.push(response);
+              createOfflineData(result);
+              return next(null);
+            });
+          })
         });
       });
 
